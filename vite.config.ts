74bd6269e283afa3e50a88b95a550689fa0a72ea
ErrQD0303 <quocdat3396@gmail.com>
2025-03-29 14:39:46 +0700

    Add https support for frontend

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync("localhost-key.pem"),
+      cert: fs.readFileSync("localhost-cert.pem"),
+    },
+    port: 5173,
+  },
   plugins: [react()],
   css: {
     postcss: {},
